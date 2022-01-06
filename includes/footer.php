f46bca55039a2eea92a4faff9fedf213d74ca54b
Miguelito-13 <68496218+Miguelito-13@users.noreply.github.com>
2022-01-06 18:27:43 +0800

    created footer

---
+++ includes/footer.php
@@ -0,0 +1,126 @@
+<div id="footer"><!-- footer Start -->
+
+  <div class="container"><!-- container Start -->
+
+    <div class="row"><!-- row Start -->
+
+      <div class="col-sm-6 col-md-3"><!-- col-sm-6 Start -->
+
+        <h4>Pages</h4>
+
+        <ul>
+          <li><a href="cart.php">Shopping Cart</a></li>
+          <li><a href="contact.php">Contact Us</a></li>
+          <li><a href="shop.php">Shop</a></li>
+          <li><a href="cehckout.php">My Account</a></li>
+        </ul>
+
+        <hr>
+
+        <h4>User Section</h4>
+
+        <ul>
+          <li><a href="checkout.php">Login</a></li>
+          <li><a href="customer_register.php">Sign Up</a></li>
+        </ul>
+
+        <hr class="hidden-md hidden-lg hidden-sm">
+
+      </div><!-- col-sm-6 Finish -->
+
+      <div class="col-sm-6 col-md-3"><!-- col-sm-6 Start -->
+
+        <h4>Top Product Categories</h4>
+
+        <ul>
+          <li><a href="#">Jackets</a></li>
+          <li><a href="#">Accessories</a></li>
+          <li><a href="#">Coats</a></li>
+          <li><a href="#">Shoes</a></li>
+          <li><a href="#">T-Shirts</a></li>
+        </ul>
+
+        <hr class="hidden-md hidden-lg hidden-sm">
+
+      </div><!-- col-sm-6 Finish -->
+
+      <div class="col-sm-6 col-md-3"><!-- col-sm-6 Start -->
+
+        <h4>Find Us:</h4>
+
+        <p>
+          <strong>Fake Axie Inc.</strong>
+          <br/>Manila
+          <br/>Cavite
+          <br/>8700-12-34
+          <br/>realemail.gmail.com
+          <br/><strong>Mr. Yoso</strong>
+        </p>
+
+        <a href="contact.php">Check our Contacts Page</a>
+
+        <hr class="hidden-md hidden-lg hidden-sm">
+
+      </div><!-- col-sm-6 Finish -->
+
+      <div class="col-sm-6 col-md-3"><!-- col-sm-6 Start -->
+
+        <h4>Get The News</h4>
+
+        <p>
+          Don't miss our latest showbiz happenings.
+        </p>
+
+        <form action="" method="post"><!-- form Start -->
+
+          <div class="input-group"><!-- input-group Start -->
+
+            <input type="text" class="form-control" name="email">
+
+            <span class="input-group-btn"><!-- input-group-btn Start -->
+
+              <input type="submit" value="subscribe" class="btn btn-default">
+
+            </span><!-- input-group-btn Finish -->
+
+          </div><!-- input-group Finish -->
+
+        </form><!-- form Finish -->
+
+        <h4>Keep In Touch</h4>
+
+        <p class="social">
+          <a href="#" class="fa fa-facebook"></a>
+          <a href="#" class="fa fa-twitter"></a>
+          <a href="#" class="fa fa-instagram"></a>
+          <a href="#" class="fa fa-google-plus"></a>
+          <a href="#" class="fa fa-envelope"></a>
+        </p>
+
+      </div><!-- col-sm-6 Finish -->
+
+    </div><!-- row Finish -->
+
+  </div><!-- container Finish -->
+
+</div><!-- footer Finish -->
+
+<div id="copyright"><!-- copyright Start -->
+
+  <div class="container"><!-- container Start -->
+
+    <div class="col-md-6"><!-- col-md-6 Start -->
+
+      <p class="pull-left">&copy; 2022 Fake Axie Inc. All Rights Reserved</p>
+
+    </div><!-- col-md-6 Finish -->
+
+    <div class="col-md-6"><!-- col-md-6 Start -->
+
+      <p class="pull-right"> Theme by: <a href="#">Mr. Yoso</a></p>
+
+    </div><!-- col-md-6 Finish -->
+
+  </div><!-- container Finish -->
+
+</div><!-- copyright Finish -->
--- index.php
+++ index.php
@@ -425,6 +425,11 @@
 
   </div><!-- container Finish -->
 
+  <?php
+  
+    include("includes/footer.php")
+
+  ?>
 
     <script src="js/jquery-331.min.js"></script>
     <script src="js/bootstrap-337.min.js"></script>
