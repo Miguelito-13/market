de761a51e94d2cd9afa60996b7a77df59a999c5e
Miguelito-13 <68496218+Miguelito-13@users.noreply.github.com>
2022-01-19 18:28:06 +0800

    update shop.php and product category tabs

--- shop.php
+++ shop.php
@@ -74,65 +74,64 @@
                 }else{
 
                   $page = 1;
+
+                }
                   
-                  $start_from = ($page-1) * $per_page;
+                $start_from = ($page-1) * $per_page;
 
-                  $get_products = "select * from products order by 1 DESC LIMIT $start_from,$per_page";
+                $get_products = "select * from products order by 1 DESC LIMIT $start_from,$per_page";
 
-                  $run_products = mysqli_query($conn,$get_products);
+                $run_products = mysqli_query($conn,$get_products);
 
-                  while($row_products=mysqli_fetch_array($run_products)){
+                while($row_products=mysqli_fetch_array($run_products)){
 
-                    $pro_id = $row_products['product_id'];
+                  $pro_id = $row_products['product_id'];
 
-                    $pro_name = $row_products['product_name'];
+                  $pro_name = $row_products['product_name'];
 
-                    $pro_price = $row_products['product_price'];
+                  $pro_price = $row_products['product_price'];
 
-                    $pro_img1 = $row_products['product_img1'];
+                  $pro_img1 = $row_products['product_img1'];
 
-                    echo "
+                  echo "
                     
-                      <div class='col-md-4 col-sm-6 center-responsive'>
+                    <div class='col-md-4 col-sm-6 center-responsive'>
 
-                        <div class='product'>
+                      <div class='product'>
 
-                          <a href='details.php?pro_id=$pro_id'> 
+                        <a href='details.php?pro_id=$pro_id'> 
                           
-                            <img class='img-responsive' src='admin_area/product_images/$pro_img1'></img> 
+                          <img class='img-responsive' src='admin_area/product_images/$pro_img1'></img> 
                           
-                          </a>
+                        </a>
 
-                          <div class='text'>
+                        <div class='text'>
                           
-                            <h3> <a href='details.php?pro_id=$pro_id'>$pro_name</a> </h3>
+                          <h3> <a href='details.php?pro_id=$pro_id'>$pro_name</a> </h3>
                           
-                            <p class='price'>P$pro_price</p>
+                          <p class='price'>P$pro_price</p>
 
-                            <p class='button'>
+                          <p class='button'>
                             
-                              <a class='btn btn-default' href='details.php?pro_id=$pro_id'> View Details </a>
+                            <a class='btn btn-default' href='details.php?pro_id=$pro_id'> View Details </a>
 
-                              <a class='btn btn-primary' href='details.php?pro_id=$pro_id'> 
+                            <a class='btn btn-primary' href='details.php?pro_id=$pro_id'> 
                               
-                                <i class='fa fa-shopping-cart'></i> Add to Cart 
+                              <i class='fa fa-shopping-cart'></i> Add to Cart 
                               
-                              </a>
+                            </a>
 
-                            </p>
-
-                          </div>
+                          </p>
 
                         </div>
 
                       </div>
+
+                    </div>
                     
-                    ";
+                  ";
 
-                  }
-
-                }
-
+                }   
           ?>
 
         </div><!-- row Finish -->
@@ -142,6 +141,48 @@
           <ul class="pagination">
             
             <?php
+
+                  $query = "select * from products";
+
+                  $result = mysqli_query($conn,$query);
+
+                  $total_records = mysqli_num_rows($result);
+
+                  $total_pages = ceil($total_records / $per_page);
+
+                  echo "
+                  
+                    <li>
+                    
+                      <a href='shop.php?page=1'>".'First Page'."</a>
+
+                    </li>
+                  
+                  ";
+
+                  for($i=1; $i<=$total_pages; $i++){
+
+                    echo "
+                  
+                      <li>
+                      
+                        <a href='shop.php?page=".$i."'> ".$i." </a>
+
+                      </li>
+                  
+                    ";
+
+                  };
+
+                  echo "
+                  
+                    <li>
+                    
+                      <a href='shop.php?page=$total_pages'>".'Last Page'."</a>
+
+                    </li>
+                  
+                  ";
             
                 }
 
@@ -152,6 +193,8 @@
           </ul>
 
         </center>
+
+        <?php getpcatpro(); ?>
 
       </div><!-- col-md-9 Finish -->
 
