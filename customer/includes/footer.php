3ada1e0bb66aa48c2057d2945d6e8f923c9419e9
Miguelito-13 <68496218+Miguelito-13@users.noreply.github.com>
2022-01-19 14:45:17 +0800

    update footer.php and created header.php

--- customer/includes/footer.php
+++ customer/includes/footer.php
@@ -9,10 +9,10 @@
         <h4>Pages</h4>
 
         <ul>
-          <li><a href="../cart.php">Shopping Cart</a></li>
-          <li><a href="../contact.php">Contact Us</a></li>
-          <li><a href="../shop.php">Shop</a></li>
-          <li><a href="my_account.php">My Account</a></li>
+          <li><a href="cart.php">Shopping Cart</a></li>
+          <li><a href="contact.php">Contact Us</a></li>
+          <li><a href="shop.php">Shop</a></li>
+          <li><a href="customer/my_account.php">My Account</a></li>
         </ul>
 
         <hr>
@@ -21,7 +21,7 @@
 
         <ul>
           <li><a href="checkout.php">Login</a></li>
-          <li><a href="../customer_register.php">Sign Up</a></li>
+          <li><a href="customer_register.php">Sign Up</a></li>
         </ul>
 
         <hr class="hidden-md hidden-lg hidden-sm">
@@ -33,11 +33,37 @@
         <h4>Top Product Categories</h4>
 
         <ul>
-          <li><a href="#">Jackets</a></li>
-          <li><a href="#">Accessories</a></li>
-          <li><a href="#">Coats</a></li>
-          <li><a href="#">Shoes</a></li>
-          <li><a href="#">T-Shirts</a></li>
+          
+          <?php
+          
+            $get_p_cats = "select * from product_categories";
+
+            $run_p_cats = mysqli_query($conn,$get_p_cats);
+
+            while($row_p_cats=mysqli_fetch_array($run_p_cats)){
+              
+              $p_cat_id = $row_p_cats['p_cat_id'];
+
+              $p_cat_name = $row_p_cats['p_cat_name'];
+
+              echo "
+              
+                <li>
+
+                  <a href='shop.php?p_cat=$p_cat_id'>
+                  
+                    $p_cat_name
+                  
+                  </a>
+
+                </li>
+              
+              ";
+      
+            }
+          
+          ?>
+
         </ul>
 
         <hr class="hidden-md hidden-lg hidden-sm">
@@ -57,7 +83,7 @@
           <br/><strong>Mr. Yoso</strong>
         </p>
 
-        <a href="../contact.php">Check our Contacts Page</a>
+        <a href="contact.php">Check our Contacts Page</a>
 
         <hr class="hidden-md hidden-lg hidden-sm">
 
