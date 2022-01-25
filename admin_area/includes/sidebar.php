a365209f7a8b5dcdb7b5b6e405fe8747f2b831f5
Miguelito-13 <68496218+Miguelito-13@users.noreply.github.com>
2022-01-25 12:09:45 +0800

    created admin dashboard

---
+++ admin_area/includes/sidebar.php
@@ -0,0 +1,194 @@
+<nav class="navbar navbar-inverse navbar-fixed-top"><!-- navbar Start -->
+
+  <div class="navbar-header"><!-- navbar-header Start -->
+
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-exl-collapse">
+
+      <span class="sr-only">Toggle Navigation</span>
+
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+
+    </button>
+
+    <a href="index.php?dashboard" class="navbar-brand">Admin Area</a>
+
+  </div><!-- navbar-header Finish -->
+
+  <ul class="nav navbar-right top-nav">
+
+    <li class="dropdown">
+
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+        <i class="fa fa-user"></i> Mr. Yoso <b class="caret"></b>
+
+      </a>
+
+      <ul class="dropdown-menu">
+
+        <li>
+          <a href="index.php?user_profile">
+            <i class="fa fa-fw fa-user"></i> Profile
+          </a>
+        </li>
+
+        <li>
+          <a href="index.php?view_products">
+            <i class="fa fa-fw fa-envelope"></i> Products
+            <span class="badge">7</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="index.php?view_customers">
+            <i class="fa fa-fw fa-users"></i> Customers
+            <span class="badge">11</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="index.php?view_cats">
+            <i class="fa fa-fw fa-gear"></i> Product Categories
+            <span class="badge">4</span>
+          </a>
+        </li>
+
+        <li class="divider"></li>
+
+        <li>
+          <a href="logout.php">
+            <i class="fa fa-fw fa-power-off"></i> Logout
+          </a>
+        </li>
+
+      </ul>
+
+    </li>
+
+  </ul>
+
+  <div class="collapse navbar-collapse navbar-exl-collapse"><!-- collapse Start -->
+
+    <ul class="nav navbar-nav side-nav">
+
+      <li>
+        <a href="index.php?dashboard">
+          <i class="fa fa-fw fa-dashboard"></i> Dashboard
+        </a>
+
+      </li>
+
+      <li>
+        <a href="#" data-toggle="collapse" data-target="#products">
+          <i class="fa fa-fw fa-tag"></i> Products
+          <i class="fa fa-fw fa-caret-down"></i> 
+        </a>
+
+        <ul id="products" class="collapse">
+          <li>
+            <a href="index.php?insert_product"> Insert Product </a>
+          </li>
+          <li>
+            <a href="index.php?view_products"> View Products </a>
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="#" data-toggle="collapse" data-target="#p_cat">
+          <i class="fa fa-fw fa-edit"></i> Product Categories
+          <i class="fa fa-fw fa-caret-down"></i> 
+        </a>
+
+        <ul id="p_cat" class="collapse">
+          <li>
+            <a href="index.php?insert_p_cat"> Insert Product Category</a>
+          </li>
+          <li>
+            <a href="index.php?view_p_cats"> View Product Categories </a>
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="#" data-toggle="collapse" data-target="#cat">
+          <i class="fa fa-fw fa-book"></i> Categories
+          <i class="fa fa-fw fa-caret-down"></i> 
+        </a>
+
+        <ul id="cat" class="collapse">
+          <li>
+            <a href="index.php?insert_cat"> Insert Category</a>
+          </li>
+          <li>
+            <a href="index.php?view_cats"> View Categories </a>
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="#" data-toggle="collapse" data-target="#slide">
+          <i class="fa fa-fw fa-gear"></i> Slides
+          <i class="fa fa-fw fa-caret-down"></i> 
+        </a>
+
+        <ul id="slide" class="collapse">
+          <li>
+            <a href="index.php?insert_slide"> Insert Slide</a>
+          </li>
+          <li>
+            <a href="index.php?view_slides"> View Slides </a>
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="index.php?view_customers">
+          <i class="fa fa-fw fa-users"></i> View Customers
+        </a>
+      </li>
+
+      <li>
+        <a href="index.php?view_oders">
+          <i class="fa fa-fw fa-book"></i> View Orders
+        </a>
+      </li>
+
+      <li>
+        <a href="index.php?view_payments">
+          <i class="fa fa-fw fa-money"></i> View Payments
+        </a>
+      </li>
+
+      <li>
+        <a href="#" data-toggle="collapse" data-target="#users">
+          <i class="fa fa-fw fa-users"></i> Users
+          <i class="fa fa-fw fa-caret-down"></i> 
+        </a>
+
+        <ul id="users" class="collapse">
+          <li>
+            <a href="index.php?insert_user"> Insert User</a>
+          </li>
+          <li>
+            <a href="index.php?view_users"> View Users </a>
+          </li>
+          <li>
+            <a href="index.php?user_profile"> Edit User Profile </a>
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="logout.php">
+          <i class="fa fa-fw fa-power-off"></i> Logout
+        </a>
+      </li>
+
+    </ul>
+
+  </div><!-- collapse Finish -->
+
+</nav><!-- navbar Finish -->
---
+++ admin_area/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Area</title>
+  <link rel="stylesheet" href="css/bootstrap-337.min.css">
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+  <div id="wrapper"><!-- wrapper Start -->
+
+    <?php include("includes/sidebar.php"); ?>
+
+    <div id="page-wrapper"><!-- page-wrapper Start -->
+
+      <div class="container-fluid"><!-- container-fluid Start -->
+
+
+
+      </div><!-- container-fluid Finish -->
+
+    </div><!-- page-wrapper Finish -->
+
+  </div><!-- wrapper Finish -->
+
+  <script src="js/jquery-331.min.js"></script>
+  <script src="js/bootstrap-337.min.js"></script>
+
+</body>
+</html>
